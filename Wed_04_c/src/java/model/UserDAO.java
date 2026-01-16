1e85138e844e079a6f272a6216959c19acdcaa51
AnVu05 <vudinhan2k5@gmail.com>
2026-01-16 20:09:21 +0700

    Wed_04_c

---
+++ Wed_04_c/src/java/model/UserDAO.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.DBContext;
+
+/**
+ *
+ * @author AN
+ */
+public class UserDAO extends DBContext {
+
+    ArrayList<UserDTO> list = new ArrayList<>();
+
+    public UserDTO searchByUsername(String username) {
+        connection = getConnection();
+        String sql = "SELECT * FROM [dbo].[tblUsers]\n"
+                + "  where userID =?";
+        UserDTO user = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String userId = resultSet.getString("userID");
+                String fullname = resultSet.getString("fullName");
+                String password = resultSet.getString("password");
+                String role = resultSet.getString("roleID");
+                boolean status = resultSet.getBoolean("status");
+                user = new UserDTO(userId, fullname, password, role, status);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+        System.out.println(user.toString());
+        return user;
+    }
+
+    public UserDTO login(String username, String password) {
+        UserDTO u = searchByUsername(username);
+        if (u != null && u.getPassword().equals(password)) {
+            return u;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Wed_04_c/web/login.jsp
@@ -0,0 +1,45 @@
+<%-- 
+    Document   : login
+    Created on : Jan 8, 2026, 11:08:32 AM
+    Author     : AN
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="MainController" method="Post">
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>Username</td>
+                        <td>
+                            <input type="text" name="username">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>
+                            <input type="password" name="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="submit" name="action" value="login">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+        <%
+            String message = request.getAttribute("message") + "";
+            message = message.equals("null")? "":message;
+        %>
+        <span style="color: red"><%=message%></span>
+    </body>
+</html>
