57c5da9fc40b3d2f1b4cf8cd53f34f6c00e538b0
AnVu05 <vudinhan2k5@gmail.com>
2026-01-22 20:59:55 +0700

    Wed_06_a

---
+++ Wed_06_a/src/java/dal/UniversityDAO.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.University;
+
+/**
+ *
+ * @author AN
+ */
+public class UniversityDAO extends DBContext {
+
+    public ArrayList<University> getAll() {
+        ArrayList<University> result = new ArrayList<>();
+        connection = getConnection();
+        String sql = "SELECT *\n"
+                + "  FROM [PRJ30x_DB1].[dbo].[tblUniversity]\n";
+        try {
+            statement = connection.prepareStatement(sql);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String shortName = resultSet.getString("shortName");
+                String description = resultSet.getString("description");
+                int foundedYear = resultSet.getInt("foundedYear");
+                String address = resultSet.getString("address");
+                String city = resultSet.getString("city");
+                String region = resultSet.getString("region");
+                String type = resultSet.getString("type");
+                int totalStudents = resultSet.getInt("totalStudents");
+                int totalFaculties = resultSet.getInt("totalFaculties");
+                boolean Draft = resultSet.getBoolean("isDraft");
+                University u = new University(id, name, shortName, description, foundedYear, address, city, region, type, totalStudents, totalFaculties, Draft);
+                result.add(u);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UniversityDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public ArrayList<University> searchByColum(String colum, String value) {
+        ArrayList<University> result = new ArrayList<>();
+        connection = getConnection();
+        String sql = "SELECT *\n"
+                + "  FROM [PRJ30x_DB1].[dbo].[tblUniversity]\n"
+                + "  where "+ colum + " = ?";
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, value);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String shortName = resultSet.getString("shortName");
+                String description = resultSet.getString("description");
+                int foundedYear = resultSet.getInt("foundedYear");
+                String address = resultSet.getString("address");
+                String city = resultSet.getString("city");
+                String region = resultSet.getString("region");
+                String type = resultSet.getString("type");
+                int totalStudents = resultSet.getInt("totalStudents");
+                int totalFaculties = resultSet.getInt("totalFaculties");
+                boolean Draft = resultSet.getBoolean("isDraft");
+                University u = new University(id, name, shortName, description, foundedYear, address, city, region, type, totalStudents, totalFaculties, Draft);
+                result.add(u);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UniversityDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public ArrayList<University> filterByColum(String colum, String value) {
+        ArrayList<University> result = new ArrayList<>();
+        connection = getConnection();
+        String sql = "SELECT *\n"
+                + "  FROM [PRJ30x_DB1].[dbo].[tblUniversity]\n"
+                + "  where "+ colum + " like ?";
+        
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, "%" + value + "%");
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String shortName = resultSet.getString("shortName");
+                String description = resultSet.getString("description");
+                int foundedYear = resultSet.getInt("foundedYear");
+                String address = resultSet.getString("address");
+                String city = resultSet.getString("city");
+                String region = resultSet.getString("region");
+                String type = resultSet.getString("type");
+                int totalStudents = resultSet.getInt("totalStudents");
+                int totalFaculties = resultSet.getInt("totalFaculties");
+                boolean Draft = resultSet.getBoolean("isDraft");
+                University u = new University(id, name, shortName, description, foundedYear, address, city, region, type, totalStudents, totalFaculties, Draft);
+                result.add(u);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UniversityDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public ArrayList<University> searchByID(String ID) {
+        return searchByColum("id", ID);
+    }
+
+    public ArrayList<University> searchByName(String name) {
+        return searchByColum("name", name);
+    }
+
+    public ArrayList<University> searchByShortName(String shortName) {
+        return searchByColum("shortName", shortName);
+    }
+
+    public ArrayList<University> filterByName(String name) {
+        return filterByColum("name", name);
+    }
+}
---
+++ Wed_06_a/web/search.jsp
@@ -0,0 +1,95 @@
+<%-- 
+    Document   : search
+    Created on : Jan 22, 2026, 9:50:16 AM
+    Author     : AN
+--%>
+
+<%@page import="model.User"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="model.University"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <%
+            User user = (User) session.getAttribute("userWasLogin");
+            if (user != null) {
+            } else {
+                response.sendRedirect("login.jsp");
+            }
+        %>
+        <!-- From -->
+        <form action="MainController" method="Get">
+            <table border="0">
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="text" name="keyword">
+                            <input type="hidden" name="action" value="search">
+                        </td>
+                        <td>
+                            <input type="submit" value="Search">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+        <!-- Display list University -->
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Short Name</th>
+                    <th>Description</th>
+                    <th>Founded Year</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>Region</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <%            
+                    if (request.getAttribute("listUniversity") != null) {
+                        ArrayList<University> list = (ArrayList<University>) request.getAttribute("listUniversity");
+                        if (!list.isEmpty()) {
+                            for (University u : list) {
+                %>
+                <tr>
+                    <td><%=u.getId()%></td>
+                    <td><%=u.getName()%></td>
+                    <td><%=u.getShortName()%></td>
+                    <td><%=u.getDescription()%></td>
+                    <td><%=u.getFoundedYear()%></td>
+                    <td><%=u.getAddress()%></td>
+                    <td><%=u.getCity()%></td>
+                    <td><%=u.getRegion()%></td>
+                    <td>
+                        <%
+                            if (u.isDraft()) {                                
+                        %>
+                        <input type="button" name="name" value="Edit">
+                        <%        
+                            }
+                        %>
+                    </td>
+                </tr>
+                <%
+                    }
+                } else {
+                %>
+            <p style="color: red">This University do not exit!</p>
+            <%        
+                    }
+                }
+            %>
+        </tbody>
+    </table>
+</body>
+</html>
